02fa5fcb519027659eb2c39a69da6fb0b71f9051
rohan17088 <44478017+rohan17088@users.noreply.github.com>
2020-02-25 19:12:28 +0530

    Added comments for KDA.py

--- KDA.py
+++ KDA.py
@@ -5,42 +5,30 @@ import socket
 s = socket.socket()          
 print ("Socket successfully created")
   
-# reserve a port on your computer in our 
-# case it is 12345 but it can be anything 
-port = 12345 
-               
-  
-# Next bind to the port 
-# we have not typed any ip in the ip field 
-# instead we have inputted an empty string 
-# this makes the server listen to requests  
-# coming from other computers on the network 
+
+port = 1001
 s.bind(('', port))         
 print ("socket binded to %s" %(port)) 
   
-# put the socket into listening mode 
+
 s.listen()      
 print ("socket is listening")            
   
-# Establish connection with client. 
+# Establish connection with A. 
 c, addr = s.accept()      
 print ('Connected to A')  	
-
 message = str(c.recv(1024))
 print (message)
 c.send(b'Sending public key of B')
-# Close the connection with the client 
- 
+# Close the connection with the A
 c.close()
 
+
+# Establish connection with A. 
 c, addr = s.accept()      
 print ('Connected to B')  	
-
 message = str(c.recv(1024))
 print (message)
 c.send(b'Sending public key of A')
-
+# Close the connection with the A
 c.close()
-
-
-
