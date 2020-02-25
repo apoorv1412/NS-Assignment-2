3acbe113222e71a088a9f3e8e2084b77702d3cdb
Appy1412 <apoorv17027@iiitd.ac.in>
2020-02-25 19:10:01 +0530

    Added comments for A.py

--- A.py
+++ A.py
@@ -1,57 +1,39 @@
-import socket                
-  
-# Create a socket object 
-s = socket.socket()          
-  
-# Define the port on which you want to connect 
-port = 12345                
-  
-# connect to the server on local computer 
-s.connect(('127.0.0.1', port)) 
-  
-# receive data from the server 
+import socket         
+
+'''
+Sending request to KDA asking for public key of B
+'''       
+s = socket.socket()            
+port = 1001             
+s.connect(('192.168.59.49', port))
 s.send(b'Connection request of B')
 message = s.recv(1024)
 print (message)
 print ('Got public key of B') 
-
-# close the connection 
 s.close()
 
-port_b = 12346
-
-s.connect(('127.0.0.1', port))
-
+'''
+Sending message to B, asking for confirmation
+'''
+port = 2001
+s = socket.socket()
+s.connect(('192.168.32.232', port))
 s.send(b'Sending message to B')
 s.close()
 
-
-# reserve a port on your computer in our 
-# case it is 12345 but it can be anything 
+'''
+Listening to B's response
+'''
 port = 12345 
-               
-  
-# Next bind to the port 
-# we have not typed any ip in the ip field 
-# instead we have inputted an empty string 
-# this makes the server listen to requests  
-# coming from other computers on the network 
+s = socket.socket()
 s.bind(('', port))         
 print ("socket binded to %s" %(port)) 
-  
-# put the socket into listening mode 
 s.listen()      
 print ("socket is listening")            
-  
-# Establish connection with client. 
 c, addr = s.accept()      
 print ('Connected to B') 
-
 print ('received confirmation from B')
-
 c.send(b'Sending confirmation to B')
-
 message = str(c.recv(1024))
 print (message)
-
 c.close()
