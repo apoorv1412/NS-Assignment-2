fa6d3e0fa25bbe1443e303c4ddb8f22729c306d0
Apoorv Singh <40769379+Appy1412@users.noreply.github.com>
2020-02-26 15:18:10 +0530

    Update KDA.py

--- KDA.py
+++ KDA.py
@@ -1,40 +1,47 @@
 # first of all import the socket library 
 import socket, RSA
 
+def format(msg):
+	return msg.split('||')
+
 private_key_KDA = 1001629
 public_key_A = 1893307107
 public_key_B = 1841622505
-
-
-
   
 s = socket.socket()          
 print ("Socket successfully created")
   
-
-port = 1001
+port = 10002
 s.bind(('', port))         
 print ("socket binded to %s" %(port)) 
   
-
 s.listen()      
 print ("socket is listening")            
   
 # Establish connection with A. 
 c, addr = s.accept()      
 print ('Connected to A')  	
-message = str(c.recv(1024))
-print (message)
-c.send(b'Sending public key of B')
+request = c.recv(1024).decode()
+formatted_request = format(request)[0]
+print (formatted_request)
+print ('Sending public key of B')
+message = str(public_key_B) + '||' + request
+encrypted_message = RSA.encrypt(message, RSA.n, private_key_KDA)
+c.send(str.encode(encrypted_message))
 # Close the connection with the A
 c.close()
 
-
-# Establish connection with A. 
+# Establish connection with B. 
 c, addr = s.accept()      
 print ('Connected to B')  	
-message = str(c.recv(1024))
-print (message)
-c.send(b'Sending public key of A')
+request = c.recv(1024).decode()
+formatted_request = format(request)[0]
+print (formatted_request)
+print ('Sending public key of A')
+message = str(public_key_A) + '||' + request
+encrypted_message = RSA.encrypt(message, RSA.n, private_key_KDA)
+c.send(str.encode(encrypted_message))
 # Close the connection with the A
 c.close()
+
+s.close()
