dd3020cc6729d11cb2ce24ab8222fc4323a52368
rohan17088 <44478017+rohan17088@users.noreply.github.com>
2020-02-26 15:52:25 +0530

    Update A.py

--- A.py
+++ A.py
@@ -14,7 +14,7 @@ def format(msg):
 Sending request to KDA asking for public key of B
 '''       
 s = socket.socket()            
-port = 10002           
+port = 10002        
 s.connect(('192.168.32.218', port))
 message = 'Connection request of B' + '||' + '.'
 # message = RSA.encrypt(message, RSA.n, )
@@ -38,19 +38,26 @@ encrypted_request_to_B = RSA.encrypt(request_to_B, RSA.n, public_key_B)
 s.send(str.encode(encrypted_request_to_B))
 s.close()
 
-# '''
-# Listening to B's response
-# '''
-# port = 12345 
-# s = socket.socket()
-# s.bind(('', port))         
-# print ("socket binded to %s" %(port)) 
-# s.listen()      
-# print ("socket is listening")            
-# c, addr = s.accept()      
-# print ('Connected to B') 
-# print ('received confirmation from B')
+'''
+Listening to B's response
+'''
+port = 2002 
+s = socket.socket()
+s.bind(('', port))         
+print ("socket binded to %s" %(port)) 
+s.listen()      
+print ("socket is listening")            
+c, addr = s.accept()      
+print ('Connected to B') 
+print ('received confirmation from B')
 # c.send(b'Sending confirmation to B')
-# message = str(c.recv(1024))
-# print (message)
-# c.close()
+confirmation_from_B = c.recv(1024).decode()
+decrypted_confirmation_from_B = RSA.decrypt(confirmation_from_B, RSA.n, private_key_A)
+print('----',decrypted_confirmation_from_B)
+nonce_2 = format(decrypted_confirmation_from_B)[1]
+print ('nonce_2', nonce_2)
+
+encrypted_confirmation_to_B = RSA.encrypt(nonce_2, RSA.n, public_key_B)
+c.send(str.encode(encrypted_confirmation_to_B))
+c.close()
+s.close()
