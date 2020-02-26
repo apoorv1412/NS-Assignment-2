87da61b07ee979e03fe70acb8b3bc0abc01f9c1a
rohan17088 <44478017+rohan17088@users.noreply.github.com>
2020-02-26 14:32:29 +0530

    Update A.py

--- A.py
+++ A.py
@@ -1,8 +1,8 @@
-import socket
+import socket, RSA
 
-public_key_A = 1893307107
 private_key_A = 100015691
-n = 2055803977
+public_key_KDA = 199570405
+
 
 
 '''
@@ -11,34 +11,37 @@ Sending request to KDA asking for public key of B
 s = socket.socket()            
 port = 1001             
 s.connect(('192.168.59.49', port))
-s.send(b'Connection request of B')
-message = s.recv(1024)
-print (message)
+message = 'Connection request of B'
+# message = RSA.encrypt(message, RSA.n, )
+s.send(str.encode(message))
+reply = s.recv(1024)
+reply = str.decode(reply)
+print (reply)
 print ('Got public key of B') 
 s.close()
 
 '''
 Sending message to B, asking for confirmation
 '''
-port = 2001
-s = socket.socket()
-s.connect(('192.168.32.232', port))
-s.send(b'Sending message to B')
-s.close()
+# port = 2001
+# s = socket.socket()
+# s.connect(('192.168.32.232', port))
+# s.send(b'Sending message to B')
+# s.close()
 
-'''
-Listening to B's response
-'''
-port = 12345 
-s = socket.socket()
-s.bind(('', port))         
-print ("socket binded to %s" %(port)) 
-s.listen()      
-print ("socket is listening")            
-c, addr = s.accept()      
-print ('Connected to B') 
-print ('received confirmation from B')
-c.send(b'Sending confirmation to B')
-message = str(c.recv(1024))
-print (message)
-c.close()
+# '''
+# Listening to B's response
+# '''
+# port = 12345 
+# s = socket.socket()
+# s.bind(('', port))         
+# print ("socket binded to %s" %(port)) 
+# s.listen()      
+# print ("socket is listening")            
+# c, addr = s.accept()      
+# print ('Connected to B') 
+# print ('received confirmation from B')
+# c.send(b'Sending confirmation to B')
+# message = str(c.recv(1024))
+# print (message)
+# c.close()
